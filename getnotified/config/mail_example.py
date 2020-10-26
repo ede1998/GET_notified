3905bc2bc4ce063113cb95da4e08cbf45fcaaae1
ede1998 <online@erik-hennig.me>
2020-10-26 15:57:51 +0100

    ADD: config example

---
+++ getnotified/config/mail_example.py
@@ -0,0 +1,7 @@
+smtp_server = 'smtp.dns.tld'
+smtp_port = 587
+user = 'example@dns.tld'
+sender = 'example@dns.tld'
+password = 'password'
+
+receivers = ['receiver1@mail.example', 'receiver2@mail.example']
